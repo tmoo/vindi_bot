5cbcf3d67db4da4c6c2612200b6eb45743e25119
Tuomo Lehtonen <tuomo.lehtonen@helsinki.fi>
2017-03-31 22:14:32 +0300

    method for turning the single string board of GameState to a 2d array of tiles

--- src/main/java/bot/TheBot.java
+++ src/main/java/bot/TheBot.java
@@ -1,20 +1,32 @@
 package bot;
 
 import dto.GameState;
+import java.util.Arrays;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * The bot I will be working on. At the moment it chooses an action randomly.
- * 
+ *
  * @author tuomo
  */
 public class TheBot implements Bot {
 
+    private static final Logger logger = LogManager.getLogger(SimpleBotRunner.class);
+
+    /**
+     * 
+     * @param gameState
+     * @return The next move of the bot.
+     */
     @Override
     public BotMove move(GameState gameState) {
-        System.out.println(gameState);
-        int randomNumber = (int)(Math.random() * 4);
 
-        switch(randomNumber) {
+        String[][] board = readBoardIntoArray(gameState);
+
+        int randomNumber = (int) (Math.random() * 4);
+
+        switch (randomNumber) {
             case 1:
                 return BotMove.NORTH;
             case 2:
@@ -28,6 +40,32 @@ public class TheBot implements Bot {
         }
     }
 
+    /**
+     * 
+     * @param gameState
+     * @return The board in array for easy manipulation.
+     */
+    private String[][] readBoardIntoArray(GameState gameState) {
+        int sizeOfBoard = gameState.getGame().getBoard().getSize();
+        String boardString = gameState.getGame().getBoard().getTiles();
+        String[][] board = new String[sizeOfBoard][sizeOfBoard];
+        int charsInBoard = sizeOfBoard * sizeOfBoard * 2;
+
+        logger.info(sizeOfBoard);
+
+        int j = 0;
+        for (int i = 0; i < charsInBoard - 1; i += 2) {
+            if (i != 0 && i % (2 * sizeOfBoard) == 0) {
+                j++;
+            }
+            String tile = Character.toString(boardString.charAt(i))
+                    + Character.toString(boardString.charAt(i + 1));
+            board[j][(i / 2) % sizeOfBoard] = tile;
+        }
+
+        return board;
+    }
+
     @Override
     public void setup() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -37,5 +75,5 @@ public class TheBot implements Bot {
     public void shutdown() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
