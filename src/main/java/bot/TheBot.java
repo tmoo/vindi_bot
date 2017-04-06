580faa7e8aa630948d15e3e54f884742d533579b
Tuomo Lehtonen <tuomo.lehtonen@helsinki.fi>
2017-04-06 15:27:32 +0300

    fixed dangerzone so

--- src/main/java/bot/TheBot.java
+++ src/main/java/bot/TheBot.java
@@ -4,6 +4,7 @@ import dto.GameState;
 import dto.GameState.Hero;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -236,7 +237,7 @@ public class TheBot implements Bot {
         boolean danger = false;
         for (Hero h : heroes) {
             if (h.getPos().getX() == i && h.getPos().getY() == j) {
-                if (h.getLife() >= hero.getLife()) {
+                if (hero.getId() != h.getId() && h.getLife() >= hero.getLife()) {
                     danger = true;
                 }
             }
