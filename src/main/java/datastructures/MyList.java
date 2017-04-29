89934e7c6db3fe3a42ee2d8f6e1a8bacb8bb7243
Tuomo Lehtonen <tuomo.lehtonen@cs.helsinki.fi>
2017-04-30 00:29:09 +0300

    comments

--- src/main/java/datastructures/MyList.java
+++ src/main/java/datastructures/MyList.java
@@ -26,16 +26,29 @@ public class MyList<E> implements List<E> {
         this(INITIAL_CAPACITY);
     }
 
+    /**
+     *
+     * @return The number of elements in this list.
+     */
     @Override
     public int size() {
         return cursor;
     }
 
+    /**
+     *
+     * @return true if there are no elements in the list, false otherwise.
+     */
     @Override
     public boolean isEmpty() {
         return cursor < 1;
     }
 
+    /**
+     *
+     * @param o The object which whose containment is checked.
+     * @return True if o is contained in this list.
+     */
     @Override
     public boolean contains(Object o) {
         for (int i = 0; i < cursor; i++) {
@@ -54,6 +67,10 @@ public class MyList<E> implements List<E> {
         return false;
     }
 
+    /**
+     *
+     * @return Iterator over the list.
+     */
     @Override
     public Iterator<E> iterator() {
         Iterator<E> iter;
@@ -79,16 +96,35 @@ public class MyList<E> implements List<E> {
         return iter;
     }
 
+    /**
+     *
+     * @return An array with the elements of this list and length equal to the
+     * number of elements.
+     */
     @Override
     public Object[] toArray() {
         return Arrays.copyOf(array, cursor);
     }
 
+    /**
+     * Not supported.
+     *
+     * @param <T>
+     * @param arg0
+     * @return
+     */
     @Override
     public <T> T[] toArray(T[] arg0) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Add the element e in the list. Grow the size of the inner array if
+     * necessary.
+     *
+     * @param e Element to be added.
+     * @return True in any case.
+     */
     @Override
     public boolean add(E e) {
         if (cursor >= array.length) {
@@ -101,6 +137,11 @@ public class MyList<E> implements List<E> {
         return true;
     }
 
+    /**
+     *
+     * @param o Object to be removed.
+     * @return True if the element was contained in the list, false otherwise.
+     */
     @Override
     public boolean remove(Object o) {
         for (int i = 0; i < cursor; i++) {
@@ -121,6 +162,11 @@ public class MyList<E> implements List<E> {
         return false;
     }
 
+    /**
+     *
+     * @param c Collection whose containment is checked.
+     * @return True if all elements in c are also contained in this list.
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
         if (c == null) {
@@ -134,46 +180,91 @@ public class MyList<E> implements List<E> {
         return true;
     }
 
+    /**
+     * Not supported.
+     * @param c
+     * @return 
+     */
     @Override
     public boolean addAll(Collection<? extends E> c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param index
+     * @param c
+     * @return 
+     */
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param c
+     * @return 
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param c
+     * @return 
+     */
     @Override
     public boolean retainAll(Collection<?> c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     */
     @Override
     public void clear() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param index
+     * @return 
+     */
     @Override
     public E get(int index) {
         return (E) array[index];
     }
 
+    /**
+     * Not supported.
+     * @param index
+     * @param element
+     * @return 
+     */
     @Override
     public E set(int index, E element) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param index
+     * @param element 
+     */
     @Override
     public void add(int index, E element) {
-
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param index
+     * @return 
+     */
     @Override
     public E remove(int index) {
         E e = (E) array[index];
@@ -182,26 +273,51 @@ public class MyList<E> implements List<E> {
         return e;
     }
 
+    /**
+     * Not supported.
+     * @param o
+     * @return 
+     */
     @Override
     public int indexOf(Object o) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param o
+     * @return 
+     */
     @Override
     public int lastIndexOf(Object o) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @return 
+     */
     @Override
     public ListIterator<E> listIterator() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param index
+     * @return 
+     */
     @Override
     public ListIterator<E> listIterator(int index) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not supported.
+     * @param fromIndex
+     * @param toIndex
+     * @return 
+     */
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
